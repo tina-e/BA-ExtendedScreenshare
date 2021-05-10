d9caa4fe2286db4bf47d547632e0fe5727b464b3
tina-e <tina.emmert@web.de>
2021-05-10 15:15:03 +0200

    fixed file-sender/receiver

--- receiver.py
+++ receiver.py
@@ -1,12 +1,12 @@
 import socket
 import os
 
-server_host = "192.168.178.169"
+server_host = "0.0.0.0"
 # or 0.0.0.0 for more than 1 network
 server_port = 5000
 
 SEPARATOR = "<SEP>"
-BUFFER_SIZE = 4096
+BUFFER_SIZE = 4096 # TODO: problem mit kleinen Dateien -> nichts kommt an
 
 server_socket = socket.socket()
 server_socket.bind((server_host, server_port))
@@ -18,7 +18,10 @@ client_socket, address = server_socket.accept()
 print(f"{address} is connected.")
 
 received = client_socket.recv(BUFFER_SIZE).decode()
-filename = os.path.basename(received.split(SEPARATOR)[0])
+filename = received.split(SEPARATOR)[0]
+#filename, filesize = received.split(SEPARATOR)
+# idk y but filesize braucht man hier, sonst kommt kein Inhalt an
+filename = os.path.basename(filename)
 
 with open(filename, "wb") as file:
     while True:
