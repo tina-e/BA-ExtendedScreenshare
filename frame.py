a24ebc415d31f04be76787aaacbea096e3b3045a
tina-e <tina.emmert@web.de>
2021-06-02 13:42:57 +0200

    working: transparent window

--- frame.py
+++ frame.py
@@ -1,116 +1,50 @@
+import Xlib.Xatom
 from Xlib import X, display
-import Xlib.xobject.colormap as colormap
-from Xlib.X import CWColormap, CWBorderPixel, CWBackPixel, CWEventMask
-import xutil
+
+# https://github.com/alexer/python-xlib-render/blob/86fc1a20292a7d3c913e63b3600da7f09e7b58ff/xutil.py
+def match_visual_info(screen, depth, visual_class):
+	for depth_info in screen.allowed_depths:
+		if depth_info.depth != depth:
+			continue
+		for visual_info in depth_info.visuals:
+			if visual_info.visual_class != visual_class:
+				continue
+			return visual_info.visual_id
+
+x = 2500
+y = 100
+width = 500
+height = 350
+border_width = 50
+
+rects = [(x, y, width, border_width), (x, y, border_width, height), (x, height-border_width, width, border_width), (width-border_width, y, border_width, height)]
+
+dpy = display.Display()
+srn = dpy.screen()
+root = srn.root
+
+depth = 32
+visual = match_visual_info(srn, depth, X.TrueColor)
+colormap = root.create_colormap(visual, X.AllocNone)
+colormap.alloc_color(255, 1, 1)
 
 
-class Window:
-    def __init__(self, display, msg):
-        self.display = display
-        self.msg = msg
+window = root.create_window(x, y, width, height, border_width,
+                            depth, X.InputOutput, visual,
+                            event_mask=X.StructureNotifyMask | X.CWColormap | X.CWBorderPixel | X.CWBackPixel,
+                            border_pixel=0, background_pixel=0, colormap=colormap)
 
-        self.screen = self.display.screen()
-        self.window = self.screen.root.create_window(
-            10, 10, 100, 100, 1,
-            self.screen.root_depth,
-            background_pixel=self.screen.white_pixel,
-            event_mask=X.ExposureMask | X.KeyPressMask,
-        )
+# event_mask=X.StructureNotifyMask|X.ExposureMask|X.PropertyChangeMask|X.EnterWindowMask|X.LeaveWindowMask|X.KeyRelease|X.ButtonPress|X.ButtonRelease|X.KeymapStateMask
+#X.CWColormap | X.CWBorderPixel | X.CWBackPixel
+gc = window.create_gc()
 
+wm_delete_window = dpy.intern_atom("WM_DELETE_WINDOW", 0)
+window.set_wm_protocols([wm_delete_window], 1)
 
+window.map()
 
-        #rq.Pixmap('background_pixmap'),
-        #rq.Card32('background_pixel'),
-        #rq.Pixmap('border_pixmap'),
-        #rq.Card32('border_pixel'),
-        #rq.Gravity('bit_gravity'),
-        #rq.Gravity('win_gravity'),
-        #rq.Set('backing_store', 1,
-        #       (X.NotUseful, X.WhenMapped, X.Always)),
-        #rq.Card32('backing_planes'),
-        #rq.Card32('backing_pixel'),
-        #rq.Bool('override_redirect'),
-        #rq.Bool('save_under'),
-        #rq.Card32('event_mask'),
-        #rq.Card32('do_not_propagate_mask'),
-        #rq.Colormap('colormap'),
-        #rq.Cursor('cursor'),
-        #)
-
-
-        self.gc = self.window.create_gc(
-            foreground=self.screen.black_pixel,
-            background=self.screen.white_pixel,
-        )
-
-        self.window.map()
-
-    def loop(self):
-        while True:
-            e = self.display.next_event()
-
-            if e.type == X.Expose:
-                self.window.fill_rectangle(self.gc, 20, 20, 10, 10)
-                self.window.draw_text(self.gc, 10, 50, self.msg)
-            elif e.type == X.KeyPress:
-                raise SystemExit
-
-
-class Frame:
-    def __init__(self, display, x, y, width, height, border_width):
-        self.display = display
-        self.screen = self.display.screen()
-
-
-        #self.screen.allowed_depths.visuals.visual_id = 0
-        #self.screen.allowed_depths.visuals.visual_class = X.TrueColor
-
-        #self.colormap = self.screen.root.create_colormap()
-        #attr.colormap = XCreateColormap(display, DefaultRootWindow(display), vinfo.visual, AllocNone)
-        colormap = self.screen.default_colormap
-        #foreground_pixel = colormap.alloc_color(0, 0, 10000).pixel
-        #background_pixel = colormap.alloc_color(0, 0, 10000).pixel
-
-        #self.visual_info = self.screen.root.match_visual_info()
-        #X. = self.screen.root.create_colormap(visual=X.StaticGray, alloc=1)
-
-        #X.ParseColor(self.display, self.visual_info, "#FF8000")
-        #X.TrueColor(self.display, self.visual_info, "#FF8000")
-
-        self.window = self.screen.root.create_window(
-            x, y, width, height, border_width,
-            self.screen.root_depth,
-            X.InputOutput,
-            visual=X.TrueColor,
-            event_mask=X.CWColormap | X.CWBorderPixel | X.CWBackPixel | CWEventMask,
-        )
-
-        #foreground_pixel = colormap.alloc_named_color(foreground_color).pixel
-
-        self.gc = self.window.create_gc()
-        wm_delete_window = self.display.intern_atom("WM_DELETE_WINDOW", 0)
-        self.window.change_property("_NET_WM_WINDOW_TYPE_TYPE", 32, X.PropModeReplace, wm_delete_window)
-        self.window.set_wm_protocols(wm_delete_window, 1)
-
-        self.window.map()
-
-
-    def loop(self):
-        while True:
-            e = self.display.next_event()
-
-            if e.type == X.ClientMessage:
-                if e.xclient.message_type == self.display.intern_atom("WM_PROTOCOLS", 1) and e.xclient.data.l[0] == self.display.intern_atom("WM_DELETE_WINDOW", 1):
-                    continue
-                #self.display.set_foreground(self.gc,)
-                #self.display.fill_rectangles(self.gc, )
-                #self.display.fill_rectangles(self.gc, )
-                #self.window.sync(self.gc, False)
-            elif e.type == X.KeyPress:
-                raise SystemExit
-
-
-
-if __name__ == "__main__":
-    #Window(display.Display(), "Hello, World!").loop()
-    Frame(display.Display(), 0, 0, 100, 100, 1).loop()
+while True:
+    e = dpy.next_event()
+    if e.type == X.ClientMessage:
+        if e.xclient.message_type == dpy.intern_atom("WM_PROTOCOLS", 1) and e.xclient.data.l[0] == dpy.intern_atom("WM_DELETE_WINDOW", 1):
+            break
