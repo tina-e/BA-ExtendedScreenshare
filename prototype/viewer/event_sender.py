68779d37146bc5262f9213409a68b34686e8e369
tina-e <tina.emmert@web.de>
2021-07-20 15:55:35 +0200

    fixed typo

--- prototype/viewer/event_sender.py
+++ prototype/viewer/event_sender.py
@@ -7,7 +7,7 @@ from pynput.mouse import Listener as MouseListener, Controller as MouseControlle
 from pynput.keyboard import Key, Listener as KeyListener, Controller as KeyController
 from event_types import EventTypes, get_id_by_button
 import window_manager
-from window_manager_test import WindowManager
+#from window_manager_test import WindowManager
 import Config
 
 
@@ -40,7 +40,7 @@ class EventSender:
         message += is_viewing.to_bytes(1, 'big')
         print(message)
         self.send(message)
-        window_manager.setup()
+
 
     def on_click(self, x, y, button, was_pressed):
         if window_manager.is_in_focus():
