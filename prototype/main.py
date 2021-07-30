0136d21d0054d7b524479b1c77950c01492875b1
tina-e <tina.emmert@web.de>
2021-07-30 15:01:15 +0200

    placed stream in qt window

--- prototype/main.py
+++ prototype/main.py
@@ -8,6 +8,11 @@ import Config
 import subprocess
 import signal
 
+from viewer.stream_window import StreamWindow
+from gi.repository import GObject
+from PyQt5.QtWidgets import *
+import sys
+
 
 streamer = None
 viewer = None
@@ -27,8 +32,13 @@ if Config.IS_STREAMER:
     streamer = Streamer()
 else:
     #signal.signal(signal.SIGINT, close)
-    viewer = Viewer()
+    GObject.threads_init()
+    app = QApplication(sys.argv)
+    viewer = Viewer(app)
     viewer.access_stream()
+    app.exec_()
+    close()
+    sys.exit(0)
 
 signal.signal(signal.SIGINT, close)
 while True:
--- prototype/viewer/viewer.py
+++ prototype/viewer/viewer.py
@@ -2,24 +2,36 @@
 
 import Config
 from viewer.event_sender import EventSender
+from viewer.stream_window import StreamWindow
+from gi.repository import GObject
+from PyQt5.QtWidgets import *
+import sys
 import subprocess
 
 class Viewer:
-    def __init__(self):
+    def __init__(self, app):
         print("Viewer alive")
         self.stream = None
         self.event_sender = EventSender()
+        '''GObject.threads_init()
+        app = QApplication(sys.argv)
+        self.event_sender = EventSender()
+        sys.exit(app.exec_())'''
 
     def access_stream(self):
         #print("Accessor benachrichten!")
-        self.stream = subprocess.Popen(f"gst-launch-1.0 -v udpsrc address={Config.RECEIVER_ADDRESS} port={Config.STREAM_PORT} "
+        print("stream is goin to be opened")
+        self.stream = StreamWindow()
+
+        '''self.stream = subprocess.Popen(f"gst-launch-1.0 -v udpsrc address={Config.RECEIVER_ADDRESS} port={Config.STREAM_PORT} "
                          "caps = \"application/x-rtp, media=(string)video, "
                          "clock-rate=(int)90000, encoding-name=(string)H264, "
-                         "payload=(int)96\" ! rtph264depay ! decodebin ! videoconvert ! autovideosink", shell=True)
+                         "payload=(int)96\" ! rtph264depay ! decodebin ! videoconvert ! autovideosink", shell=True)'''
 
         self.event_sender.on_view(True)
 
 
     def end_stream(self):
-        self.stream.terminate()
+        #self.stream.terminate()
+        self.stream.close()
         self.event_sender.on_view(False)
