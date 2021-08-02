ce9db7685b4002c582697df6ad2070af58933ee8
tina-e <tina.emmert@web.de>
2021-08-02 14:35:43 +0200

    added functionality to tray

--- prototype/gui_qt.py
+++ prototype/gui_qt.py
@@ -3,6 +3,10 @@ from PyQt5.QtWidgets import QApplication, QSystemTrayIcon, QMenu, QWidget, QActi
 from PyQt5.QtGui import QIcon
 from PyQt5 import QtCore
 
+import Config
+from streamer.streamer import Streamer
+from viewer.stream_window import StreamWindow
+
 class Menu(QSystemTrayIcon):
     def __init__(self, icon, parent):
         QSystemTrayIcon.__init__(self, icon, parent)
@@ -20,18 +24,25 @@ class Menu(QSystemTrayIcon):
         menu.addSeparator()
         quit_action = menu.addAction("Quit")
         
-        fullscreen_action.triggered.connect(self.fullscreen)
+        fullscreen_action.triggered.connect(self.setup_stream)
         area_action.triggered.connect(self.area)
         play_action.triggered.connect(self.play)
         pause_action.triggered.connect(self.pause)
         access_action.triggered.connect(self.access)
         quit_action.triggered.connect(self.quit)
 
-    def fullscreen(self):
-        print("fullscreen")
+    def setup_stream(self):
+        print("setup stream")
+        Config.IS_STREAMER = True
+        Config.set_ips()
+        streamer = Streamer()
 
     def area(self):
         print("area")
+        self.stream_app.exec()
+        dimensions = self.stream_app.get_coords()
+        Config.set_coords(dimensions)
+        self.setup_stream()
 
     def play(self):
         print("play")
@@ -41,6 +52,9 @@ class Menu(QSystemTrayIcon):
 
     def access(self):
         print("access")
+        Config.IS_STREAMER = False
+        Config.set_ips()
+        stream_viewer = StreamWindow()
 
     def quit(self):
         print("end")
