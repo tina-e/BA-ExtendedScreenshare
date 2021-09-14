599e163af42dfd1b23537165ca355decf8a9ce93
tina-e <tina.emmert@web.de>
2021-09-14 14:29:23 +0200

    added timeout for clipboard

--- src/streamer/clipboard_handler.py
+++ src/streamer/clipboard_handler.py
@@ -69,8 +69,8 @@ class ClipboardHandler:
         :param incoming_content: received content that needs to be pasted
         '''
         stored_is_file, stored_content = self.read_clipboard()
-        pyclip.clear()
         pyclip.copy(incoming_content)
+        time.sleep(3)
         self.mouse_handler.simulate_paste()
         self.write_clipboard(stored_is_file, stored_content)
 
